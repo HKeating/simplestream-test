fb898f9020b2d671e218564aa2341e9d4b87c13d
HKeating <horacekeating@hotmail.co.uk>
2020-07-07 16:49:36 +0100

    Add initial video player component and implement it in Video view

---
+++ src/components/VideoPlayer.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <video ref="videoPlayer" class="video-js"></video>
+  </div>
+</template>
+
+<script>
+import 'video.js/dist/video-js.css';
+import videojs from 'video.js';
+
+export default {
+  name: 'VideoPlayer',
+  props: {
+    videoData: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      player: null,
+      options: {
+        poster: this.videoData.poster,
+        sources: [{ src: this.videoData.video }],
+        fluid: true,
+        controls: true,
+      },
+    };
+  },
+  mounted() {
+    this.player = videojs(this.$refs.videoPlayer, this.options);
+  },
+  beforeDestroy() {
+    if (this.player) {
+      this.player.dispose();
+    }
+  },
+};
+</script>
--- src/views/Video.vue
+++ src/views/Video.vue
@@ -2,12 +2,18 @@
   <div class="video">
     <h1>Video</h1>
     <h2>{{ videoData.title }}</h2>
+    <video-player :videoData="videoData" />
   </div>
 </template>
 
 <script>
+import VideoPlayer from '@/components/VideoPlayer';
+
 export default {
   name: 'Video',
+  components: {
+    VideoPlayer,
+  },
   props: {
     videoData: {
       type: Object,
